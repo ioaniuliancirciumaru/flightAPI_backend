87d83a8c9a15b28507187a5715fa05547dc12005
AlexandruOriean <alexandru.oriean@gmail.com>
2020-10-06 21:58:02 +0300

    added UML diagram

---
+++ src/main/java/com/codecool/flight_api_project/flight/FlightModel.java
@@ -0,0 +1,17 @@
+package com.codecool.flight_api_project.flight;
+
+import java.util.Date;
+
+public class FlightModel
+{
+    private String flightNumber;
+    private String departureAirport;
+    private String arrivalAirport;
+    private Date departureDate;
+    private Date arrivalDate;
+    private double price;
+    private Airline airline;
+
+
+
+}
---
+++ src/main/java/com/codecool/flight_api_project/flight/FlightRepository.java
@@ -0,0 +1,5 @@
+package com.codecool.flight_api_project.flight;
+
+public class FlightRepository
+{
+}
