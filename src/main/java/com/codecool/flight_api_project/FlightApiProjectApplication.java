c17178b22651188ac49be2ba5b1d6a579c724b2c
AlexandruOriean <alexandru.oriean@gmail.com>
2020-10-06 21:25:21 +0300

    initalizing spring boot backend

---
+++ src/main/java/com/codecool/flight_api_project/FlightApiProjectApplication.java
@@ -0,0 +1,15 @@
+package com.codecool.flight_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightApiProjectApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(FlightApiProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/flight_api_project/FlightApiProjectApplicationTests.java
@@ -0,0 +1,15 @@
+package com.codecool.flight_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightApiProjectApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
