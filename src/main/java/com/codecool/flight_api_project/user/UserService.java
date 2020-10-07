8da3dbf47fad49d88e274e5278799c8c4a106c02
sorinobreja <sorinobreja93@gmail.com>
2020-10-07 22:53:48 +0300

    added an example for read json

---
+++ src/main/java/com/codecool/flight_api_project/user/UserService.java
@@ -0,0 +1,27 @@
+package com.codecool.flight_api_project.user;
+
+
+import com.codecool.flight_api_project.flight.FlightModel;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+
+    public void addUser(User user) throws IOException {
+        userRepository.insertUser(user);
+    }
+
+    public List<User> list() throws IOException {
+        return userRepository.findAll();
+    }
+}
