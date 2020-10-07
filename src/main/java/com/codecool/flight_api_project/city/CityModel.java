5be9f3a3eb9bd904e7ebf770f5a10a762b4f0b22
AlexandruOriean <alexandru.oriean@gmail.com>
2020-10-07 19:54:35 +0300

    Updated city model acording to the UML

--- src/main/java/com/codecool/flight_api_project/city/CityModel.java
+++ src/main/java/com/codecool/flight_api_project/city/CityModel.java
@@ -4,33 +4,61 @@ import com.codecool.flight_api_project.airport.AirportModel;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class CityModel {
 
+    private UUID id;
     private String name;
     private List<AirportModel> airports = new ArrayList<>();
+    private String country;
 
-    public CityModel(String name, List<AirportModel> airports) {
+    public CityModel(String name, List<AirportModel> airports, String country)
+    {
+        this.id = UUID.randomUUID();
         this.name = name;
         this.airports = airports;
+        this.country = country;
     }
 
     public CityModel() {
     }
 
-    public String getName() {
+    public UUID getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
         return name;
     }
 
-    @Override
-    public String toString() {
-        return "CityModel{" +
-                "name='" + name + '\'' +
-                ", airports=" + airports +
-                '}';
+    public List<AirportModel> getAirports()
+    {
+        return airports;
     }
 
-    public List<AirportModel> getAirports() {
-        return airports;
+    public String getCountry()
+    {
+        return country;
+    }
+
+    public void addAirport(AirportModel airport)
+    {
+        this.airports.add(airport);
+        System.out.println("Airport: " + airport + " added to airports list");
+    }
+
+
+    @Override
+    public String toString()
+    {
+        return "CityModel{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", airports=" + airports +
+                ", country='" + country + '\'' +
+                '}';
     }
 }
